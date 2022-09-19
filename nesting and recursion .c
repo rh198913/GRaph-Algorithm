a1c3d18f16aca6636ccfea0e758805f72c0f1111
恬恬 <109257015+rh198913@users.noreply.github.com>
2022-09-19 22:43:16 +0800

    Update nesting and recursion .c

--- nesting and recursion .c
+++ nesting and recursion .c
@@ -13,6 +13,7 @@
     printf("%d\n",strlen("taylor"));//省略掉返回len，直接将strlen("taylor"）做为printf函数的参数
     return 0;
 } */
+/*
 //接收一个整形值（无符号），按照顺序打印它的每一位。例如：1234 打印1 2 3 4
 void print(int n)//函数可以打印参数部分数字的每一位
 {//print函数，是需求输出的一系列参数
@@ -32,7 +33,39 @@ int main()
     //print(12)34
     //print(1)234
     return 0;
+}*/
+//编写函数不允许创建临时变量，求字符串的长度,模拟实现strlen
+/*
+my_strlen(char* str)//字符的地址
+{
+    int count = 0;//此处引用了变量
+    while(*str != '\0')//判断是否是最后一个字符
+    {
+        count++;
+        str++;
+    }
+    return count;
 }
+int main()
+{
+    char arr[] = "taylor";
+    printf("%d\n",my_strlen(arr));
+    return 0;
+}
+*/
+/*此处为本题大事化小的思路，无变量
+//my_strlen(char* str)//字符的地址
+{
+    if(*str != '\0')
+        return 1 + my_strlen(str+1);//str+1表长度示下一个字符串的 + 左1是总长度。
+    else
+        return 0;
+}
+/* my_strlen("abc")
+1+my_strlen("bc")
+1+1+my_strlen("c")
+1+1+1+my_strlen("")
+1+1+1+0 */*/
 
 
 
